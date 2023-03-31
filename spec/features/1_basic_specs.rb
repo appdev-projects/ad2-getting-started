215c66703d27a813bf2a3599c79a4073661fe147
Ben Purinton <ben.purinton@gmail.com>
2023-03-31 09:29:06 -0800

    update basic test

--- spec/features/1_basic_specs.rb
+++ spec/features/1_basic_specs.rb
@@ -1,10 +1,9 @@
 require "rails_helper"
 
-describe "The home page" do
-  it "can be visited when user is signed out", points: 0 do
-    visit "/"
-    current_url = page.current_path
-
-    expect(current_url).to eq("/")
+describe "The /movies page" do
+  it "can be visited", points: 0 do
+    visit "/movies"
+    
+    expect(page.status_code).to be(200)
   end
 end
