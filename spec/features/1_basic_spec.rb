5bb984f9792a4829b57d9b20e5d701271cc736d9
Ben Purinton <ben.purinton@gmail.com>
2023-04-04 23:20:10 +0000

    added another spec

--- spec/features/1_basic_spec.rb
+++ spec/features/1_basic_spec.rb
@@ -1,10 +1,19 @@
 require "rails_helper"
 
 describe "The /movies page" do
-  it "can be visited", points: 0 do
+  it "can be visited", points: 1 do
     visit "/movies"
     
     expect(page.status_code).to be(200),
       "Expected to visit /movies successfully, but didn't find it."
+  end
+end
+
+describe "The /insert_movies page" do
+  it "can be visited", points: 1 do
+    visit "/insert_movies"
+    
+    expect(page.status_code).to be(200),
+      "Expected to visit /insert_movies successfully, but didn't find it."
   end
 end
