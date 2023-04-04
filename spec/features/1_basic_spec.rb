fbbe9c66cba33fb24fbf639f45d0e97b32cd8472
Ben Purinton <ben.purinton@gmail.com>
2023-04-04 23:12:34 +0000

    first spec

---
+++ spec/features/1_basic_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+describe "The /movies page" do
+  it "can be visited", points: 0 do
+    visit "/movies"
+    
+    expect(page.status_code).to be(200),
+      "Expected to visit /movies successfully, but didn't find it."
+  end
+end
