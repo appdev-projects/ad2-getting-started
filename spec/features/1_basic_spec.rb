3345d34c0a700961a7cfd5dc1133ebeb2fcd83bf
Ben Purinton <ben.purinton@gmail.com>
2023-04-05 12:03:03 -0800

    movie to Movie to match helper-methods spec

--- spec/features/1_basic_spec.rb
+++ spec/features/1_basic_spec.rb
@@ -43,10 +43,10 @@ describe "The /movies/new page" do
 
     fill_in "Title", with: "My test movie"
     fill_in "Description", with: "description"
-    click_button "Create movie"
+    click_button "Create Movie"
 
     expect(page).to have_content("Movie created successfully."),
-      "Expected to fill in the new movie form, click 'Create movie', and be redirected to the movie index with a success notice"
+      "Expected to fill in the new movie form, click 'Create Movie', and be redirected to the movie index with a success notice"
   end
 end
 
@@ -63,8 +63,8 @@ describe "The movie details page" do
   it "has a link to delete the movie with a DELETE request", points: 2 do
     visit "/movies/#{movie.id}"
 
-    expect(page).to have_selector("a[href='/movies/#{movie.id}'][data-method='delete']", text: 'Delete movie'),
-      "Expected /movies/ID to have 'Delete movie' link with the proper data-method='delete'."
+    expect(page).to have_selector("a[href='/movies/#{movie.id}'][data-method='delete']", text: 'Delete Movie'),
+      "Expected /movies/ID to have 'Delete Movie' link with the proper data-method='delete'."
   end
 end
 
