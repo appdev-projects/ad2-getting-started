6817ea86f622cd666b4556450947370b31061dd1
Ben Purinton <ben.purinton@gmail.com>
2023-04-05 03:13:04 +0000

    added a bunch of specs

--- spec/features/1_basic_spec.rb
+++ spec/features/1_basic_spec.rb
@@ -1,10 +1,66 @@
 require "rails_helper"
 
 describe "The /movies page" do
+  before do
+    visit movies_path
+  end
+
   it "can be visited", points: 1 do
-    visit "/movies"
-    
     expect(page.status_code).to be(200),
-      "Expected to visit /movies successfully, but didn't find it."
+      "Expected to visit /movies successfully."
+  end
+
+  it "has a link to add a movie", points: 1 do
+    expect(page).to have_link('Add a new movie', href: movies_new_path),
+      "Expected /movies to have an 'Add a new movie' link to '/movies/new'."
+  end
+end
+
+describe "The /movies/new page" do
+  before do
+    visit movies_new_path
+  end
+
+  it "can be visited", points: 1 do
+    expect(page.status_code).to be(200),
+      "Expected to visit /movies/new successfully."
+  end
+
+  it "has a form", points: 1 do
+    expect(page).to have_selector("form[action='/movies']"),
+      "Expected /movies/new to have a form with action='/movies'."
+  end
+
+  it "has a hidden authenticity token input", points: 2 do
+    expect(page).to have_selector("input[name='authenticity_token']", visible: false),
+      "Expected the new movie form to have an input field of type='hidden' and name='authenticity_token'."
+  end
+
+  it "creates a movie successfully", point: 1 do
+    fill_in "Title", with: "My test movie"
+    fill_in "Description", with: "description"
+    click_button "Create movie"
+    expect(page).to have_content("Movie created successfully."),
+      "Expected to fill in the new movie form, click 'Create movie', and be redirected to the movie index with a success notice"
+  end
+end
+
+describe "The movie details page" do
+  before do
+    @movie = Movie.create(
+      title: "My title",
+      description: "My description"
+    )
+    visit "/movies/#{@movie.id}"
+  end
+
+  it "can be visited", points: 1 do
+    expect(page.status_code).to be(200),
+      "Expected to visit /movies/ID successfully."
+  end
+
+  it "has a link to delete the movie with a DELETE request", points: 2 do
+    expect(page).to have_selector("a[href='/movies/#{ @movie.id }'][data-method='delete']", text: 'Delete movie'),
+      "Expected /movies/ID to have 'Delete movie' link with the proper data-method='delete'."
   end
 end
