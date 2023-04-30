8030778593fcdaf126dd2e19ed42b94a707bfc33
Mohammad Ali Karimi <ali216k@gmail.com>
2023-04-30 19:27:58 +0000

    Created books table using scaffold generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_29_163502) do
+ActiveRecord::Schema.define(version: 2023_04_30_190154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "released"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
