99778d225874b2bebd23713274bf45c210a8cdfa
Tan Sakeer <tansakeer@gmail.com>
2023-05-23 22:08:03 +0000

    adding RESTful links Delete not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # Routes for the Movie resource:
+
+  # CREATE
+  post("/movies", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  
+  patch("/movies/:path_id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  delete("/movies/:path_id", { :controller => "movies", :action => "destroy" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
