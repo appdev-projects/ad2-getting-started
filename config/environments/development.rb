5b4914d81abe72993f05a5b65675852925cea10c
Ben Purinton <ben.purinton@gmail.com>
2024-04-18 08:42:35 -0700

    replace deprecated web_console config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts.clear
   # Allow all Gitpod IPs to render the BetterErrors page
-  config.web_console.whitelisted_ips = '0.0.0.0/0.0.0.0'
+  config.web_console.allowed_ips = '0.0.0.0/0.0.0.0'
   BetterErrors::Middleware.allow_ip! '0.0.0.0/0.0.0.0'
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
